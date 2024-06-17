b3a49cddf7976be9c79ce918ad13decef51733ed
mattiaBarbo <mattia2002.barbo@gmail.com>
2024-06-17 16:43:46 +0200

    Base_Pipeline

---
+++ SQAaaS_Pipeline/.sqa/script.pretty-buff-shrew.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/HPCI-Lab/yProv &&
+    flake8 .
+)
