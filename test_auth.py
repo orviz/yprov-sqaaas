93a10e6ba41c2c5df33e50996d8bf2a72155b8d2
mattiaBarbo <mattia2002.barbo@gmail.com>
2024-06-20 16:27:55 +0200

    test_auth

---
+++ test_auth.py
@@ -0,0 +1,73 @@
+import requests
+
+PATH = "http://localhost:3000/api/v0/auth"
+
+def test_register():
+    # correct new username and password 
+    payload = {
+        "user": "myUsername",
+        "password": "myPassword"
+    }
+    response = requests.post(PATH + '/register', json=payload)
+    assert response.status_code == 200
+
+    # username and password already exist
+    payload = {
+        "user": "myUsername",
+        "password": "myPassword"
+    }
+    response = requests.post(PATH + '/register', json=payload)
+    assert response.status_code == 400
+
+    # no username
+    payload = {
+        "password": "myPassword"
+    }
+    response = requests.post(PATH + '/register', json=payload)
+    assert response.status_code == 400
+
+    # no password
+    payload = {
+        "user": "myUsername"
+    }
+    response = requests.post(PATH + '/register', json=payload)
+    assert response.status_code == 400
+
+    # mi manca l'errore 401
+
+    
+def test_login():
+    # correct login
+    payload = {
+        "user": "myUsername",
+        "password": "myPassword"
+    }
+    response = requests.post(PATH + '/login', json=payload)
+    assert response.status_code == 200
+
+    # no username and password
+    payload = {
+        "user": "",
+        "password": ""
+    }
+    response = requests.post(PATH + '/login', json=payload)
+    assert response.status_code == 400
+
+    # correct username, wrong password
+    payload = {
+        "user": "myUsername",
+        "password": "wrongPassword"
+    }
+    response = requests.post(PATH + '/login', json=payload)
+    assert response.status_code == 401
+
+    # wrong username, correct password
+    payload = {
+        "user": "wrongUser",
+        "password": "myPassword"
+    }
+    response = requests.post(PATH + '/login', json=payload)
+    assert response.status_code == 401
+    
+
+
