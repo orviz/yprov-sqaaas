837bbb999b8bb6da084a43c438d2d43d2ada3543
mattiaBarbo <mattia2002.barbo@gmail.com>
2024-07-04 12:45:09 +0200

    error correction

--- test_auth.py
+++ test_auth.py
@@ -1,100 +1,18 @@
-import os
 import requests
 import test_documents
 
-PATH = "/api/v0/auth"
-
-
-def test_auth_login():
-    # Obtain the ngrok URL dynamically from GitHub Actions environment
-    ngrok_url = os.getenv('NGROK_URL')  
-
-    # correct login
-    payload = {
-        "user": "myUsername",
-        "password": "myPassword"
-    }
-    response = requests.post(ngrok_url + "/api/v0/auth/login", json=payload)
-    assert response.status_code == 200
-
-    test_documents.TOKEN = response.json().get("result")
-
-    # no username and password
-    payload = {
-        "user": "",
-        "password": ""
-    }
-    response = requests.post(ngrok_url + "/api/v0/auth/login", json=payload)
-    assert response.status_code == 400
-
-    # correct username, wrong password
-    payload = {
-        "user": "myUsername",
-        "password": "wrongPassword"
-    }
-    response = requests.post(ngrok_url + "/api/v0/auth/login", json=payload)
-    assert response.status_code == 401
-
-    # wrong username, correct password
-    payload = {
-        "user": "wrongUser",
-        "password": "myPassword"
-    }
-    response = requests.post(ngrok_url + "/api/v0/auth/login", json=payload)
-    assert response.status_code == 401
-
-"""   
-def test_auth_login():
-    # correct login
-    payload = {
-        "user": "myUsername",
-        "password": "myPassword"
-    }
-    response = requests.post(PATH + '/login', json=payload)
-    assert response.status_code == 200
-
-    test_documents.TOKEN = response.json().get("result")
-
-    # no username and password
-    payload = {
-        "user": "",
-        "password": ""
-    }
-    response = requests.post(PATH + '/login', json=payload)
-    assert response.status_code == 400
-
-    # correct username, wrong password
-    payload = {
-        "user": "myUsername",
-        "password": "wrongPassword"
-    }
-    response = requests.post(PATH + '/login', json=payload)
-    assert response.status_code == 401
-
-    # wrong username, correct password
-    payload = {
-        "user": "wrongUser",
-        "password": "myPassword"
-    }
-    response = requests.post(PATH + '/login', json=payload)
-    assert response.status_code == 401
-"""
-    
-    
-
-
-
+PATH = "http://localhost:3000/api/v0/auth"
 
 def test_auth_register():
-    """
+    
     # correct new username and password 
     payload = {
         "user": "myUsername",
         "password": "myPassword"
     }
     response = requests.post(PATH + '/register', json=payload)
-    assert response.status_code == 200
-    """
+    assert response.status_code == 201
+    
 
     # username and password already exist
     payload = {
@@ -119,5 +37,47 @@ def test_auth_register():
     assert response.status_code == 400
 
     # mi manca l'errore 401
+
+
+def test_auth_login():
+    # correct login
+    payload = {
+        "user": "myUsername",
+        "password": "myPassword"
+    }
+    response = requests.post(PATH + '/login', json=payload)
+    assert response.status_code == 200
+
+    test_documents.TOKEN = response.json().get("result")
+
+    # no username and password
+    payload = {
+        "user": "",
+        "password": ""
+    }
+    response = requests.post(PATH + '/login', json=payload)
+    assert response.status_code == 400
+
+    # correct username, wrong password
+    payload = {
+        "user": "myUsername",
+        "password": "wrongPassword"
+    }
+    response = requests.post(PATH + '/login', json=payload)
+    assert response.status_code == 401
+
+    # wrong username, correct password
+    payload = {
+        "user": "wrongUser",
+        "password": "myPassword"
+    }
+    response = requests.post(PATH + '/login', json=payload)
+    assert response.status_code == 401
+
+    
+
+
+
+
 
 
