165381d52d00aabd7cbb759ed6974f45365f07c1
Pablo Orviz <orviz@ifca.unican.es>
2024-07-23 13:41:38 +0200

    Use Docker hostnames

--- test_documents.py
+++ test_documents.py
@@ -3,7 +3,7 @@ import requests
 PATH = "http://localhost:3000/api/v0/documents"
 
 # path to use to create a docker image
-#PATH = "http://web:3000/api/v0/documents"
+PATH = "http://web:3000/api/v0/documents"
 
 
 TOKEN = None
